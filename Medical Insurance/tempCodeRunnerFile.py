93787ecc6a77191ea3cc727cb80a0c988973323f
Ritesh Shivankar <149803980+riteshivankar@users.noreply.github.com>
2025-03-12 00:48:12 +0530

    files added

---
+++ Medical Insurance/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+    new_data = pd.DataFrame({
+    'age': [input_data.age],
+    'sex': [input_data.sex],
+    'bmi': [input_data.bmi],
+    'children': [input_data.children],
+    'smoker': [input_data.smoker],
+    'region': [input_data.region]
+    })
